a5511e83be734103f30d2b7cf06bff77092e6ad3
rudrabarot71 <81606729+rudrabarot71@users.noreply.github.com>
2023-12-09 01:40:56 -0500

    Update training.py

--- training.py
+++ training.py
@@ -2,7 +2,7 @@
 from pyspark.sql.functions import col, isnan
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import VectorAssembler, StandardScaler
-from pyspark.ml.classification import LogisticRegression, RandomForestClassifier, DecisionTreeClassifier
+from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
 from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 from pyspark.ml import Pipeline
@@ -85,14 +85,12 @@ feature_assembler = VectorAssembler(inputCols=['fixed_acidity', 'volatile_acidit
 feature_scaler = StandardScaler(inputCol="inputFeatures", outputCol="features")
 
 # Initialize classification models
-logistic_regression = LogisticRegression()
+
 random_forest = RandomForestClassifier()
-decision_tree = DecisionTreeClassifier(labelCol="label", featuresCol="features", seed=0)
 
 # Create pipelines with different classifiers
-pipeline_lr = Pipeline(stages=[feature_assembler, feature_scaler, logistic_regression])
+
 pipeline_rf = Pipeline(stages=[feature_assembler, feature_scaler, random_forest])
-pipeline_dt = Pipeline(stages=[feature_assembler, feature_scaler, decision_tree])
 
 # Build a parameter grid for hyperparameter tuning
 param_grid = ParamGridBuilder().build()
@@ -101,27 +99,16 @@ param_grid = ParamGridBuilder().build()
 model_evaluator = MulticlassClassificationEvaluator(metricName="f1")
 
 # Cross-validate each pipeline using the specified classifiers
-cross_validator_lr = CrossValidator(estimator=pipeline_lr, estimatorParamMaps=param_grid, evaluator=model_evaluator, numFolds=10)
-cv_model_lr = cross_validator_lr.fit(df)
-print("F1 Score for LogisticRegression Model: ", model_evaluator.evaluate(cv_model_lr.transform(test_df)))
 
 cross_validator_rf = CrossValidator(estimator=pipeline_rf, estimatorParamMaps=param_grid, evaluator=model_evaluator, numFolds=10)
 cv_model_rf = cross_validator_rf.fit(df)
 print("F1 Score for RandomForestClassifier Model: ", model_evaluator.evaluate(cv_model_rf.transform(test_df)))
 
-cross_validator_dt = CrossValidator(estimator=pipeline_dt, estimatorParamMaps=param_grid, evaluator=model_evaluator, numFolds=10)
-cv_model_dt = cross_validator_dt.fit(df)
-print("F1 Score for DecisionTreeClassifier Model: ", model_evaluator.evaluate(cv_model_dt.transform(test_df)))
-
 # Save models to S3
-model_path_lr = "s3a://cldassign2/LogisticRegression"
-cv_model_lr.save(model_path_lr)
 
 model_path_rf = "s3a://cldassign2/RandomForestClassifier"
 cv_model_rf.save(model_path_rf)
 
-model_path_dt = "s3a://cldassign2/DecisionTreeClassifier"
-cv_model_dt.save(model_path_dt)
 
 # Stop the Spark session
 spark.stop()
